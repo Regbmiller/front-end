e6a7f860bab62e995054f140d05366e19b263d02
Your Name <Reginaldbmiller@gmail.com>
2020-05-24 08:54:29 -0500

    changing file structure

---
+++ front-end/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+import App from "./App";
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
